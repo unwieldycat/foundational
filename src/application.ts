95ab61d17005e37a279101611544c8bca7718d3a
unwieldycat <thurstonalexyates@gmail.com>
2021-07-20 15:06:19 -0500

    Various changes
    Run formatter and add dividers for readability
    Add more internal methods
    Remove debug garbage at bottom

--- src/application.ts
+++ src/application.ts
@@ -6,11 +6,110 @@ import { deepFreeze } from './utilities';
 // =============== Application =============== //
 
 export default function application(): Application {
+
+    // ------------ Private Properties ------------ //
+
     /** @access private */
     const _commands: Command[] = [];
 
     /** @access private */
     const _options: Option[] = [];
+
+    // -------------- Input Parsing -------------- //
+
+    /**
+     * @access private
+     * @param input
+     */
+    const _parseArguments = (spec: string, exec: string[]): Record<string, unknown> => {
+        const args = {};
+        const specMatch = spec.match(/^((?:<\w*> ?)*)(\[\w*\])?$/gm);
+        if (specMatch) {
+            const requiredKeys = specMatch[1].replace(/[[\]<>]+/g, '').trim().split(' ');
+            requiredKeys.forEach((key, index) => {
+                if (!exec[index]) throw new Error(`Missing required argument: ${key}`);
+                Object.defineProperty(args, key, { value: exec[index] });
+            });
+
+            if (specMatch[2]) {
+                // stuff
+            }
+        }
+        return args;
+    };
+
+    /**
+     * @access private
+     * @param input
+     */
+    const _parseOptions = (input: string[]): Record<string, string> => {
+        const options = {};
+
+        for (const arg of input) {
+            // im proud of this mean ass regex
+            const regexMatch = arg.match(/^(--?[\w-]*)([= ]?)((?:\w*|"[^"]*"))$/gm);
+            if (!regexMatch || regexMatch.length <= 0) continue;
+
+            const optionKey = regexMatch[1];
+            const optionValue = regexMatch[2];
+
+            const optionMeta = _options.find((e) => {
+                return e.name === optionKey || e.alias?.includes(optionKey);
+            });
+
+            Object.defineProperty(options, optionKey, {
+                value: optionMeta?.flag ? true : optionValue
+            });
+        }
+
+        return options;
+    };
+
+    // ---------------- Validation ---------------- //
+
+    /**
+     * @access private
+     * @param command
+     */
+    const _validateCommand = (command: Command): void => {
+        if (_commands.find((e) => e.name === command.name)) {
+            throw new Error(`Command ${command.name} already exists`);
+        }
+        
+        if (command.options) {
+            _validateOptions(command.options);
+            command.options.forEach((o) => {
+                if (_options.find((e) => e.name === o.name)) {
+                    throw new Error(`Option ${o.name} already exists in global options`);
+                }
+            });
+        }
+
+        if (command.arguments && !/^((?:<\w*> ?)*)(\[\w*\])?$/gm.test(command.arguments)) {
+            throw new Error(`Arguments for command ${command.name} are formatted incorrectly`);
+        }
+    };
+
+    /**
+     * @access private
+     * @param optionArray
+     */
+    const _validateOptions = (optionArray: Option[]): void => {
+        for (const option of optionArray) {
+            if (!/^--(\w*)$/.test(option.name)) {
+                throw new Error(`Option "${option.name}" has an incorrectly formatted name`);
+            }
+
+            if (!option.alias) continue;
+            for (const alias of option.alias) {
+                if (!/^-(\w)$/.test(alias)) {
+                    throw new Error(`Alias "${alias}" for option ${option.name} is formatted incorrectly`);
+                }
+            }
+        }
+    };
+
+    // -------------- Public Methods -------------- //
 
     /**
      * Register a command
@@ -18,7 +117,7 @@ export default function application(): Application {
      * @returns Command object
      */
     const command = (command: Command): void => {
-        // check command is valid
+        _validateCommand(command);
         _commands.push(command);
     };
 
@@ -33,55 +132,6 @@ export default function application(): Application {
     };
 
     /**
-     * @access private
-     * @param input
-     */
-    const _parseOptions = (input: string[]): Record<string, string> => {
-        const options = {};
-
-        for (const arg of input) {
-            // im proud of this mean ass regex
-            const regexMatch = arg.match(/^(--?[\w-]*)([= ]?)((?:\w* *|".*"?))$/gm);
-            if (!regexMatch || regexMatch.length <= 0) continue;
-
-            const optionKey = regexMatch[1];
-            const optionValue = regexMatch[2];
-
-            const optionMeta = _options.find((e) => {
-                return e.name === optionKey || e.alias?.includes(optionKey);
-            });
-
-            Object.defineProperty(options, optionKey, {
-                value: (optionMeta?.type === 'boolean') ? true : optionValue
-            });
-        }
-
-        return options;
-    };
-
-    /**
-     * @access private
-     */
-    const _validateOptions = (optionArray: Option[]): void => {
-        for (const option of optionArray) {
-            if (!/^--(\w*)$/.test(option.name)) {
-                throw new Error(
-                    `Option "${option.name}" has an incorrectly formatted name`
-                );
-            }
-
-            if (!option.alias) continue;
-            for (const alias of option.alias) {
-                if (!/^-(\w)$/.test(alias)) {
-                    throw new Error(
-                        `Alias "${alias}" for option ${option.name} is formatted incorrectly`
-                    );
-                }
-            }
-        }
-    };
-
-    /**
      * Parse console input & run
      * @param input
      */
@@ -89,6 +139,8 @@ export default function application(): Application {
         const options = _parseOptions(input);
         console.log(options);
     };
+
+    // -------------- Return Object -------------- //
 
     return deepFreeze({
         _options,
@@ -99,6 +151,3 @@ export default function application(): Application {
         run
     });
 }
-
-// @ts-ignore
-application()._parseOptions();
