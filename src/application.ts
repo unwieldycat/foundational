c07f34d6fa8bfa3fdd896eba762e98645bdd4c95
unwieldycat <thurstonalexyates@gmail.com>
2021-07-22 18:15:33 -0500

    Work on run method and improve jsdoc

--- src/application.ts
+++ src/application.ts
@@ -1,12 +1,17 @@
 // ================= Imports ================= //
 
-import { Application, Command, Option } from './interfaces';
+import { Application, ApplicationSpec, Command, Option } from './interfaces';
 import { deepFreeze, define, matchAll } from './utilities';
 import regexes from './regexes';
 
 // =============== Application =============== //
 
-export default function application(): Application {
+/**
+ * Application constructor
+ * @param {ApplicationSpec} config App configuration 
+ * @returns {Application} Application object
+ */
+export default function application(config: ApplicationSpec): Application {
     // ------------ Private Properties ------------ //
 
     const _commands: Command[] = [];
@@ -14,7 +19,7 @@ export default function application(): Application {
 
     // -------------- Input Parsing -------------- //
 
-    const _parseArguments = (spec: string, providedArgs: string[]): Record<string, unknown> => {
+    const _parseArguments = (spec: string, providedArgs: string[]): Record<string, string> => {
         const args = {};
 
         const keys = matchAll(regexes.argParse, spec);
@@ -33,8 +38,8 @@ export default function application(): Application {
             // it's the last index of the array + 1
             // (due to arrays starting at 0)
             if (optionalKey && providedArgs[requiredKeys.length]) {
-                define(args, optionalKey, providedArgs[requiredKeys.length]);    
-            }  
+                define(args, optionalKey, providedArgs[requiredKeys.length]);
+            }
         }
 
         return args;
@@ -96,8 +101,8 @@ export default function application(): Application {
 
     /**
      * Register a command
-     * @param command Command configuration
-     * @returns Command object
+     * @param { Command } command Command configuration
+     * @returns { void } void
      */
     const command = (command: Command): void => {
         _validateCommand(command);
@@ -106,8 +111,8 @@ export default function application(): Application {
 
     /**
      * Specify global options
-     * @param options Command options/configuration
-     * @returns Command object
+     * @param { Option[] } options Command options/configuration
+     * @returns { void } void
      */
     const globalOptions = (...options: Option[]): void => {
         _validateOptions(options);
@@ -116,12 +121,16 @@ export default function application(): Application {
 
     /**
      * Parse console input & run
-     * @param input
+     * @param { string[] } [input=process.argv.splice(2)] User input
      */
-    const run = (input: string[] = process.argv): void => {
+    const run = (input: string[] = process.argv.splice(2)): void => {
+        const command = _commands.find((c) => c.name === input[0]);
+        if (!command) throw new Error('Command not found');
+
         const options = _parseOptions(input);
-        //const args = _parseArguments(input);
-        console.log(options);
+        const args = _parseArguments(command.arguments || '', input);
+
+        command.action({ arguments: args, options: options });
     };
 
     // -------------- Return Object -------------- //
