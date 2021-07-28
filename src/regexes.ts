6de79d472900ece55a7fb53db46cdea4eca0d350
unwieldycat <thurstonalexyates@gmail.com>
2021-07-28 15:59:55 -0500

    Change argument regexes and add comment
    Update argParse and argValidate to allow variadic arguments

--- src/regexes.ts
+++ src/regexes.ts
@@ -1,11 +1,13 @@
+// Any important, complex, or frequently used regex goes in here
+
 // ================= Regexes ================= //
 
 const regexes = {
     optionParse: /(?<=\s|^)(--?\w[\w-]*)(?:[= ]?)(?:(\w*)|"([^"]*)")?(?<! )/gm,
     optionValidate: /^--(\w*)$/,
     aliasValidate: /^-(\w)$/,
-    argParse: /^(?:<(\w*)> ?)+(?:\[(\w*)\])?$/gm,
-    argValidate: /^(?:<\w*> ?)*(?:\[\w*\])?$/gm
+    argParse: /^((?:<\w*> ?)*)(\[\w*(?:\.\.\.)?\])?$/gm,
+    argValidate: /^(?:<\w*> ?)*(?:\[\w*(?:\.\.\.)?\])?$/gm
 };
 
 // ================= Export ================= //
