9d4cde223b604e23695db88f8a34e414a00f0641
unwieldycat <thurstonalexyates@gmail.com>
2021-07-28 20:51:45 -0500

    Fix stupidity
    Change command to option in test descriptions
    Fix incorrect regex in option name tests

--- tests/regexes.test.ts
+++ tests/regexes.test.ts
@@ -8,18 +8,18 @@ describe('test all regexes', () => {
 
     // ------------ Validation Regexes ------------ //
 
-    it('should validate proper command aliases', () => {
+    it('should validate proper option aliases', () => {
         expect(regexes.aliasValidate.test('-a')).toBe(true);
         expect(regexes.aliasValidate.test('-abc')).toBe(false);
         expect(regexes.aliasValidate.test('--abc')).toBe(false);
         expect(regexes.aliasValidate.test('-')).toBe(false);
     });
 
-    it('should validate proper command names', () => {
-        expect(regexes.aliasValidate.test('--abc')).toBe(true);
-        expect(regexes.aliasValidate.test('-abc')).toBe(false);
-        expect(regexes.aliasValidate.test('-a')).toBe(false);
-        expect(regexes.aliasValidate.test('--')).toBe(false);
+    it('should validate proper option names', () => {
+        expect(regexes.optionValidate.test('--abc')).toBe(true);
+        expect(regexes.optionValidate.test('-abc')).toBe(false);
+        expect(regexes.optionValidate.test('-a')).toBe(false);
+        expect(regexes.optionValidate.test('--')).toBe(false);
     });
 
     // ------------- Parsing Regexes ------------- //
