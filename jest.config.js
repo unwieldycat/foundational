e6271a03f9475775b48ea2649d564785cc8ce99c
unwieldycat <thurstonalexyates@gmail.com>
2021-07-28 20:48:00 -0500

    Configure to work with typescript files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  testMatch: [
+    '**/tests/*.test.ts'
+  ]
 };
