36f900c3d3a4861a33a36358eef47a7b2f89d797
unwieldycat <thurstonalexyates@gmail.com>
2021-08-08 12:23:19 -0500

    Add tests to define function and fix errors
    Remove some test cases from deepFreeze to stop errors, as the existing
    method to check if an object is frozen works fine.

--- tests/utilities.test.ts
+++ tests/utilities.test.ts
@@ -1,6 +1,6 @@
 // ================= Import ================= //
 
-import { deepFreeze } from '../src/utilities';
+import { deepFreeze, define } from '../src/utilities';
 
 // ============= Test deepFreeze ============= //
 
@@ -9,13 +9,27 @@ describe('test deepFreeze function', () => {
         const testObj = deepFreeze({ a: [1, 2, 3, [4, 5]] });
         expect(Object.isFrozen(testObj)).toBe(true);
         expect(Object.isFrozen(testObj.a)).toBe(true);
-        expect(testObj.a.push(4)).toThrow();
-        expect(testObj.a[3][1] = 6).toThrow();
     });
 
     it('should freeze nested objects', () => {
         const testObj = deepFreeze({ a: { a: 1, b: { a: 2 }}});
         expect(Object.isFrozen(testObj)).toBe(true);
         expect(Object.isFrozen(testObj.a.b)).toBe(true);
+    });
+});
+
+// =============== Test define =============== //
+
+describe('test define function', () => {
+    it('should add properties to an object', () => {
+        const testObj = {};
+        define(testObj, 'a', true);
+        expect(testObj).toStrictEqual({ a: true });
+    });
+
+    it('shouldn\'t allow adding __proto__ as a key', () => {
+        const testObj = {};
+        define(testObj, '__proto__', {});
+        expect(testObj).toStrictEqual({});
     });
 });
