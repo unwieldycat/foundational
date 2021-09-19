4eb73988c75364a6c090365ddac3ae58fa03abbc
unwieldycat <thurstonalexyates@gmail.com>
2021-09-19 19:46:15 -0400

    Add JSDoc to interface properties

--- src/types.ts
+++ src/types.ts
@@ -31,32 +31,63 @@ export interface Application {
 }
 
 export interface ApplicationSpec {
+    /** Disable the help option */
     disableHelpOption?: boolean;
 }
 
 export interface Command {
+    /** Name of command */
     name: string;
+
+    /** Description that shows up in help menu */
     description?: string;
+
+    /** Command arguments string */
     arguments?: string;
+
+    /** Command-specific options */
     options?: Array<Option>;
+
+    /** Command-specific flags */
     flags?: Array<Flag>;
+
+    /**
+     * The command's functionality
+     * @param ctx Object of command arguments and options specified by the user
+     * @returns unknown
+     */
     action: (ctx: CommandActionContext) => unknown;
 }
 
 export interface CommandActionContext {
+    /** Options and flags specified by the user */
     options: Record<string, string | boolean>;
+
+    /** Arguments specified by the user */
     arguments: Record<string, string>;
 }
 
 export interface Option {
+    /** Name of option in proper format (i.e. --option-name) */
     name: string;
+
+    /** Alias of option in proper format (i.e. -o) */
     alias?: string;
+
+    /** Description that appears in help menu */
     description: string;
+
+    /** Default value if not specified by user */
     default?: string | boolean;
 }
 
 export interface Flag {
+    /** Name of flag in proper format (i.e. --flag-name) */
     name: string;
+
+    /** Alias of option in proper format (i.e. -f) */
     alias?: string;
+    
+    /** Description that shows up in help menu */
     description?: string;
 }
